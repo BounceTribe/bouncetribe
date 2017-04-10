a1626af05565bd5b782d9a7fcd061607efcd9699
Carl Peaslee <carlpeaslee@Carls-MacBook-Pro.local>
2017-04-10 17:53:53 -0500

    more

--- src/config/index.js
+++ src/config/index.js
@@ -5,4 +5,15 @@ export const simple = 'https://api.graph.cool/simple/v1/ciwdr6snu36fj01710o4sshe
 export const fileUrl = 'https://api.graph.cool/file/v1/ciwdr6snu36fj01710o4ssheb'
 
 
-export const url = (process.env.NODE_ENV === 'production') ? 'https://www.bt-carl.com' : 'http://localhost:3000'
+export const url =  url()
+
+const url = () => {
+  switch (process.env.NODE_ENV) {
+    case 'production': {
+      return 'https://test.bouncetribe.com'
+    }
+    default: {
+      return 'http://localhost:3000'
+    }
+  }
+}
