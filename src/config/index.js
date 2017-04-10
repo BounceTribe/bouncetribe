500bf7c3d7626f3f5c06b339752cf8a6df5200d9
Carl Peaslee <carlpeaslee@Carls-MacBook-Pro.local>
2017-04-10 17:58:30 -0500

    get url fix

--- src/config/index.js
+++ src/config/index.js
@@ -5,9 +5,7 @@ export const simple = 'https://api.graph.cool/simple/v1/ciwdr6snu36fj01710o4sshe
 export const fileUrl = 'https://api.graph.cool/file/v1/ciwdr6snu36fj01710o4ssheb'
 
 
-export const url =  url()
-
-const url = () => {
+const getUrl = () => {
   switch (process.env.NODE_ENV) {
     case 'production': {
       return 'https://test.bouncetribe.com'
@@ -17,3 +15,4 @@ const url = () => {
     }
   }
 }
+export const url =  getUrl()
