8c89b745f964b5484baf08cecd1cb544d07cbcab
Carl Peaslee <cpeaslee@gmail.com>
2017-07-12 05:16:04 -0500

    rebuilding app with react router v4 and relay modern

--- src/index.js
+++ src/index.js
@@ -1,51 +1,8 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import {Router, browserHistory, applyRouterMiddleware} from 'react-router'
-import Routing from 'routing'
-import Relay from 'react-relay'
-import useRelay from 'react-router-relay'
-import {
-  RelayNetworkLayer,
-  urlMiddleware
-} from 'react-relay-network-layer'
-import {graphCool} from 'config'
-import auth from 'utils/auth'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
 
-
-
-const createHeaders = (idToken) => {
-  if (idToken) {
-    return {
-      Authorization: 'Bearer ' + idToken
-    }
-  } else {
-    return {}
-  }
-}
-
-Relay.injectNetworkLayer(
-  new RelayNetworkLayer([
-    urlMiddleware({
-      url: (req) => graphCool.relay,
-    }),
-    next => req => {
-      let idToken = auth.getToken()
-      let headers = createHeaders(idToken)
-      req.headers = {
-        ...req.headers,
-        ...headers
-      }
-      return next(req)
-    },
-  ],{ disableBatchQuery: true })
-)
-
-ReactDOM.render(
-  <Router
-    environment={Relay.Store}
-    render={applyRouterMiddleware(useRelay)}
-    history={browserHistory}
-    routes={Routing}
-  />,
-  document.getElementById('root')
-)
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
--- src/utils/strings.js
+++
@@ -1,8 +0,0 @@
-export const formatEnum = (input) => {
-  if (!input) {
-    return ''
-  } else {
-    let formatted = `${input.substring(0,1)}${input.substring(1).toLowerCase()}`
-    return formatted
-  }
-}
