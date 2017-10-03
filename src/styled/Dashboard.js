773f58c866156235024120b0257c9abaa768a170
Jake Iwen <iwenx006@umn.edu>
2017-10-03 07:02:54 -0500

    Styled Activities

---
+++ src/styled/Dashboard.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+import {purple, white} from 'theme'
+
+export const Activities = styled.div`
+  display: flex;
+  justify-content: center;
+  background-color: ${purple};
+  border-radius: 6px;
+  color: ${white};
+  padding: 4px;
+  width: 20px;
+  font-weight: 400;
+`
