018ee9560ef65f52ee3197c811d515927300b20f
Jake Iwen <iwenx006@umn.edu>
2017-10-05 15:03:39 -0500

    created add button icon

---
+++ src/icons/AddButton.js
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const AddButton = ({style, fill}) => {
+  return (
+    <svg style={style} width="40px" height="40px" viewBox="0 0 40 40" version="1.1" xmlns="http://www.w3.org/2000/svg">
+        <defs>
+            <circle id="path-1" cx="20" cy="20" r="20"></circle>
+        </defs>
+        <g id="Web-Screens" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
+            <g id="TRIBE-DASHBOARD" transform="translate(-1265.000000, -149.000000)">
+                <g id="Top" transform="translate(135.000000, 139.000000)">
+                    <g id="Header">
+                        <g id="Add-Member" transform="translate(1030.000000, 10.000000)">
+                            <g id="Add-Button" transform="translate(100.000000, 0.000000)">
+                                <use id="Oval-3" stroke="#C0C0C0" mask="url(#mask-2)" strokeWidth="2" strokeDasharray="4,5" xlinkHref="#path-1"></use>
+                                <g id="Add-Icon" transform="translate(9.000000, 12.000000)">
+                                    <ellipse id="Oval" fill="#C0C0C0" cx="12.953311" cy="3.52471047" rx="3.70094599" ry="3.52471047"></ellipse>
+                                    <path d="M12.953311,8.81177617 C7.30936833,8.81177617 5.55141899,12.3364866 5.55141899,12.3364866 L5.55141899,14.0988419 L20.3552029,14.0988419 L20.3552029,12.3364866 C20.3552029,12.3364866 18.5972536,8.81177617 12.953311,8.81177617 Z" id="Shape" fill="#C0C0C0"></path>
+                                    <path d="M3.70094599,2.64353285 L3.70094599,9.69295378" id="Shape" stroke="#C0C0C0" strokeWidth="2"></path>
+                                    <path d="M7.40189198,6.16824332 L0,6.16824332" id="Shape" stroke="#C0C0C0" strokeWidth="2"></path>
+                                </g>
+                            </g>
+                        </g>
+                    </g>
+                </g>
+            </g>
+        </g>
+    </svg>
+
+  )
+}
+export default AddButton
