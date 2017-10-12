1a228ec964664bc26e1b11c291892d91731e1039
Jake Iwen <iwenx006@umn.edu>
2017-10-11 19:20:22 -0500

    improve tabs css

---
+++ src/icons/Online.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const Online = ({size}) => {
+  return (
+    <svg width="18px" height="18px" viewBox="0 0 18 18" version="1.1" xmlns="http://www.w3.org/2000/svg">
+        <defs>
+            <circle id="path-1" cx="52" cy="52" r="7"></circle>
+        </defs>
+        <g id="Web-Screens" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
+            <g id="TRIBE-DASHBOARD" transform="translate(-508.000000, -462.000000)">
+                <g id="My-Tribe" transform="translate(135.000000, 388.000000)">
+                    <g id="Tribe-Content" transform="translate(299.000000, 0.000000)">
+                        <g id="Profile-Header" transform="translate(31.000000, 31.000000)">
+                            <g id="Oval-2">
+                                <use fill="#21D38B" fillRule="evenodd" xlinkHref="#path-1"></use>
+                                <circle stroke="#FFFFFF" strokeWidth="2" cx="52" cy="52" r="8"></circle>
+                            </g>
+                        </g>
+                    </g>
+                </g>
+            </g>
+        </g>
+    </svg>
+  )
+}
+
+export default Online
