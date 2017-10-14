4d14acb90df5e337d136368f1f5e2f4dae761ff9
Jake Iwen <iwenx006@umn.edu>
2017-10-13 20:35:25 -0500

    fixed selected user display

--- src/components/FriendList.js
+++ src/components/FriendList.js
@@ -14,14 +14,14 @@ const FriendRow = styled.div`
   width: 100%;
   padding: 8px 0;
   box-sizing: border-box;
-  border: ${props => props.selected ? `0.5px solid ${purple}` : 'none'};
 `
 const Handle = styled.span`
   flex: 1;
   padding: 0 16px;
   font-weight: 400;
   font-size: 15px;
-  color: ${props => props.active ? purple : grey600};
+  color: ${props => props.selected ? purple : grey600};
+  cursor: pointer;
 `
 
 const Header = styled.span`
@@ -33,9 +33,9 @@ const Header = styled.span`
 
 const makeRows = (users, select, selected) => (
   users.map( user =>
-    <FriendRow selected={user.id===selected.id} key={user.id} onClick={()=>select(user)}>
+    <FriendRow key={user.id} onClick={()=>select(user)}>
       <BtAvatar size={40} user={user} />
-      <Handle active={user.isOnline}>{user.handle}</Handle>
+      <Handle selected={user.id===selected.id}>{user.handle}</Handle>
       <BtTextMarker size={20} fontHeight={14} value={3}/>
     </FriendRow>
   )
