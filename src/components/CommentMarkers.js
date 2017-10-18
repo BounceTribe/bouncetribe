0058bfe120ef46332618ad1d8d2dff13a28adc6b
Matt Larson <matt.a.larson@gmail.com>
2017-10-18 10:55:32 -0500

    switched to prop-types package

--- src/components/CommentMarkers.js
+++ src/components/CommentMarkers.js
@@ -1,4 +1,5 @@
-import React, {Component, PropTypes} from 'react'
+import React, {Component} from 'react'
+import PropTypes from 'prop-types'
 import {MarkerContainer, Marker} from 'styled/Project'
 import Heart from 'icons/Heart'
 import Comment from 'icons/Comment'
