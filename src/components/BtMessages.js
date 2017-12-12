921e3f7e67299bc10c62cc16a3142de702bb79f9
ryanpowerhimself <rpowermusic@gmail.com>
2017-12-12 05:08:21 -0600

    ryan dash message styles

--- src/components/BtMessages.js
+++ src/components/BtMessages.js
@@ -1,23 +1,24 @@
 import React from 'react'
 import styled from 'styled-components'
-import {white, grey400, grey500, purple} from 'theme'
+import {white, grey300 ,grey400, grey500, grey600, purple} from 'theme'
 
 export const MsgsContainer = styled.div`
   display: flex;
   flex-direction: column-reverse;
   max-height: 50vh;
   overflow-x: scroll;
-  padding: 20px 20px 1px 20px;
-  border-bottom: 1px solid ${grey500};
+  padding: 20px 30px 1px 30px;
+  border-bottom: 1px solid ${grey300};
   bottom: 0;
 `
 const MsgBubble = styled.div`
   display: flex;
   border-radius: 5px;
-  border: 1px solid ${grey500};
+  border: 1px solid #E0E0E0;
+  box-shadow: 0 1px 3px 0 rgba(0,0,0,0.15);
   background-color: ${props => props.receiving ? white : purple};
-  color: ${props => props.receiving ? grey500 : white};
-  padding: 9px 14px;
+  color: ${props => props.receiving ? grey600 : white};
+  padding: 16px 20px;
   word-break: break-word;
   white-space: pre-wrap;
   flex: none;
@@ -26,9 +27,9 @@ const MsgTime = styled.div`
   display: flex;
   flex: none;
   align-self: ${props => props.receiving ? 'flex-start' : 'flex-end'};
-  padding: 6px 15px 13px 11px;
+  padding: 10px 15px 20px 11px;
   color: ${grey400};
-  font-size: 14px;
+  font-size: 13px;
   text-align: ${props => props.receiving ? 'left' : 'right'};
 `
 const MsgItem = styled.div`
